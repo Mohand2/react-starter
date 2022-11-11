2a9ff01c605ec449d9ddfecf0170325827704d4a
moh <alloun.mohand@gmail.com>
2022-11-11 20:34:22 +0100

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [todoList, settodoList] = useState([]);
+  const [todoItem, settodoItem] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (todoItem) {
+            settodoList([...todoList, todoItem]);
+            settodoItem('');
+          }
+        }}
+      >
+        <label htmlFor="todo"> Add a todo</label>
+        <br />
+        <input
+          id="todo"
+          type="text"
+          value={todoItem}
+          onChange={(e) => settodoItem(e.target.value)}
+        />
+        <br />
+        {todoItem && <p>Item to add : {todoItem}</p>}
+
+        <input type="submit" value="Add" />
+        <br />
+      </form>
+
+      {todoList.length > 0 && <h3>Todo List: </h3>}
+      <ul>
+        {todoList.map((todo) => {
+          return (
+            <>
+              <li> {todo}</li>
+              <input
+                type="submit"
+                value="delete"
+                onClick={() => {
+                  console.log(todo);
+                  settodoList(todoList.filter((item) => item !== todo));
+                }}
+              />
+            </>
+          );
+        })}
+      </ul>
     </div>
   );
 }
