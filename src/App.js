b871a33bbc72978ff9108442f4ba174dd68ac0ea
moh <alloun.mohand@gmail.com>
2022-11-12 18:43:02 +0100

    Create Todo Component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,7 @@
-import { useState } from 'react';
+import Todo from './components/Todo';
+
 function App() {
-  const [todoList, settodoList] = useState([]);
-  const [todoItem, settodoItem] = useState('');
-
-  return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (todoItem) {
-            settodoList([...todoList, todoItem]);
-            settodoItem('');
-          }
-        }}
-      >
-        <label htmlFor="todo"> Add a todo</label>
-        <br />
-        <input
-          id="todo"
-          type="text"
-          value={todoItem}
-          onChange={(e) => settodoItem(e.target.value)}
-        />
-        <br />
-        {todoItem && <p>Item to add : {todoItem}</p>}
-
-        <input type="submit" value="Add" />
-        <br />
-      </form>
-
-      {todoList.length > 0 && <h3>Todo List: </h3>}
-      <ul>
-        {todoList.map((todo) => {
-          return (
-            <>
-              <li> {todo}</li>
-              <input
-                type="submit"
-                value="delete"
-                onClick={() => {
-                  console.log(todo);
-                  settodoList(todoList.filter((item) => item !== todo));
-                }}
-              />
-            </>
-          );
-        })}
-      </ul>
-    </div>
-  );
+  return <Todo />;
 }
 
 export default App;
