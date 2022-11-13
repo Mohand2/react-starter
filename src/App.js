37ca4dbca98a407e50fc2f7ac4b531551497b7b5
moh <alloun.mohand@gmail.com>
2022-11-13 18:43:33 +0100

    add package uuid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Todo from './components/Todo';
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
-  return <Todo />;
+  return (
+    <>
+      <Todo />
+      <h1 >Id = {uuidv4() }</h1>
+    </>
+  );
 }
 
 export default App;
+ 
